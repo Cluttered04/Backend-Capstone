cc2ea16bd6ebd65389b1602fb11d544490c19e7f
Cluttered04 <sbpwem@gmail.com>
2019-06-26 15:05:10 -0400

    set up base models

---
+++ FairWeatherFriend/Models/Race.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FairWeatherFriend.Models
+{
+    public class Race
+    {
+        [Required]
+        public DateTime TimeOfDay { get; set; }
+        public bool isCancelled { get; set; }
+        public int Prize { get; set; }
+        [Required]
+        public int RaceTrackId { get; set; }
+        public RaceTrack Track { get; set; }
+    }
+}
